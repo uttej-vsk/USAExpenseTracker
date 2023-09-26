b7e8d980c4cadc273643f2d75eeaa47a459b84e5
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2023-09-26 20:28:16 +0530

    .

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -9,11 +9,14 @@ function Balance() {
     .reduce((acc, curr) => (acc += curr), 0)
     .toFixed(2);
 
+  const formattedSum =
+    sum < 0 ? `-$${Math.abs(sum)}` : `$${sum}`;
+
   return (
     <>
       <div className='flex flex-col mt-4'>
         <h3 className='text-l uppercase'>Your Balance</h3>
-        <h1 className='text-3xl'>${sum}</h1>
+        <h1 className='text-3xl'>{formattedSum}</h1>
       </div>
     </>
   );
